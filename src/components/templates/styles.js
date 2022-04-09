e24b03f123141f67877370d67bc13f5dc4fbfa65
munsifmulla <munsifmulla@gmail.com>
2022-04-09 18:00:16 +0530

    update: styling.

--- src/components/templates/styles.js
+++ src/components/templates/styles.js
@@ -2,10 +2,10 @@ import { makeStyles } from "@mui/styles";
 
 export default makeStyles(theme => ({
   logo: {
-    height: theme.spacing(6),
+    height: theme.spacing(9),
   },
   appHeader: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(1.75),
     background: theme.color,
     boxShadow: theme.shadowLower,
     textAlign: "center",
@@ -15,7 +15,7 @@ export default makeStyles(theme => ({
     zIndex: 2,
   },
   appBody: {
-    maxWidth: theme.spacing(180),
+    maxWidth: theme.spacing(240),
     minHeight: "110vh",
     margin: "0 auto",
     padding: theme.spacing(3),
